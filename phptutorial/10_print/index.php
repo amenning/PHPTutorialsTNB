5e049fd42aef436ef282eab9f3abb17d6c95e19a
Andrew Menning <menning23@gmail.com>
2015-09-27 16:01:29 -0400

    Lesson 10 print

---
+++ phptutorial/10_print/index.php
@@ -0,0 +1,14 @@
+<?php
+	echo "Hello World.<br />";
+	echo "Hello again.<br />";
+	
+	print "Hello World.<br />";
+	print "Hello again.<br />";
+	
+	print ("Hello World.<br />");
+	print ("Hello again.<br />");
+	
+	echo "<strong>Hello World.</strong><br />";
+	print "<strong>Hello World.</strong><br />";
+	print ("<strong>Hello World.</strong>");
+?>
