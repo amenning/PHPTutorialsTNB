39b3dbf8052492605f1400b0c3342fe6b854aa45
Andrew Menning <menning23@gmail.com>
2015-09-28 13:41:20 -0400

    Lesson 20 Assignment Operators

---
+++ phptutorial/20_Assignment_Operators/index.php
@@ -0,0 +1,29 @@
+<?php
+
+$number1 = 10;
+
+$number1 += 2; //same as $number1 = $number1 + 2;
+
+echo "$number1<br/>";
+
+$number1 -= 2; 
+
+echo "$number1<br/>";
+
+$number1 *= 2; 
+
+echo "$number1<br/>";
+
+$number1 /= 2; 
+
+echo "$number1<br/>";
+
+$number1 %= 2; 
+
+echo "$number1<br/>";
+
+$text = 'Hello';
+$text .=' World';
+echo $text;
+
+?>
