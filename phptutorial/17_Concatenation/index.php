612e5b43ebfa9f15a5d769423b945a9dbbc33a40
Andrew Menning <menning23@gmail.com>
2015-09-28 13:18:00 -0400

    Lesson 17 Concatenation

---
+++ phptutorial/17_Concatenation/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$day = 'Wednesday';
+$date = 30;
+$year = 2011;
+
+echo 'The date is <strong>'.$day.' '.$date.' '.$year.'</strong>';
+
+echo "<br />The date is <strong> $day $date $year</strong>";
+
+?>
