7aa095bbe0d848d1bd015b06243f27f660045efe
Andrew Menning <menning23@gmail.com>
2015-09-30 20:32:35 -0400

    Lesson 24 Triple Equal

---
+++ phptutorial/24_Triple_Equals/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$num1 = '1';
+$num2 = 1;
+
+if($num1==$num2){
+	echo 'Equal with ==.';
+}else{
+	echo 'Not equal with ==.';
+}
+
+if($num1===$num2){
+	echo '<br/>Equal with ===.';
+}else{
+	echo '<br/>Not equal with ===.';
+}
+
+?>
