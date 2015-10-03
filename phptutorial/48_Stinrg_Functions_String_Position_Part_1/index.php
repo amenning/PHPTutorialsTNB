c4466ade7022fc16599f40fcd64a105df47a0e9c
Andrew Menning <menning23@gmail.com>
2015-10-03 16:03:11 -0400

    Lesson 48 String Functions String Position Part 1

---
+++ phptutorial/48_Stinrg_Functions_String_Position_Part_1/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$offset = 0;
+$find = 'is';
+$find_length = strlen($find);
+
+$string = 'This is a string, and it is an example.';
+
+while($string_position = strpos($string,$find,$offset)){
+	
+}
+
+?>
