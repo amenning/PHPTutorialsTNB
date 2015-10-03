bd08b13fa583838915353477dccffd670a5873cc
Andrew Menning <menning23@gmail.com>
2015-10-03 16:42:56 -0400

    Lesson 51 String Functions Replacing Predefined Part of a String

---
+++ phptutorial/51_String_Functions_Replacing_Predefined_Part_Of_A_String/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$find = array('is','string','example');
+$replace = array('IS','STRING','');
+$string = 'This is a string, and is an example.';
+
+$new_string = str_replace($find,$replace,$string);
+
+echo $new_string;
+
+?>
