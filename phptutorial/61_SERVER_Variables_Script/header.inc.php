c2ab7f44eb0b54bb1c005f3e3e35cd12d05a5518
Andrew Menning <menning23@gmail.com>
2015-10-04 13:11:37 -0400

    Lesson 61 SERVER Variables Script

---
+++ phptutorial/61_SERVER_Variables_Script/header.inc.php
@@ -0,0 +1,7 @@
+<?php
+$script_name = $_SERVER['SCRIPT_NAME'];
+?>
+
+<form action="<?php echo $script_name;?>" method="POST">
+	<input type="submit" name="submit" value="Submit">
+</form>
---
+++ phptutorial/61_SERVER_Variables_Script/index.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'header.inc.php';
+
+if(isset($_POST['submit'])){
+	echo 'Process 1';
+}
+
+?>
