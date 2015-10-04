5c6d56b98d7579fb5c62972c4a2f3d982919238c
Andrew Menning <menning23@gmail.com>
2015-10-04 10:16:34 -0400

    Lesson 55 Creating a Find and Replace Application Part 2

---
+++ phptutorial/55_Creating_a_Find_and_Replace_Application_Part_2/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$offset = 0;
+
+if(isset($_POST['text'])&&isset($_POST['searchfor'])&&isset($_POST['replacewith'])){
+	$text = $_POST['text'];
+	$search = $_POST['searchfor'];
+	$replace = $_POST['replacewith'];
+		
+	if(!empty($text)&&!empty($search)&&!empty($replace)){
+		while($strpos = strpos($text, $search, $offset)){
+			
+		}
+		
+		
+	}else{
+		echo 'Please fill in all fields.';
+	}	
+		
+}
+
+?>
+
+<form action="index.php" method="POST">
+
+<textarea name="text" rows="6" cols="30"></textarea><br><br>
+Search for:<br>
+<input type="text" name="searchfor"><br><br>
+
+Replace with:<br>
+<input type="text" name="replacewith"><br><br>
+
+<input type="submit" value="Find and Replace">
+
+
+</form>
