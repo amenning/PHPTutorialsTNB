509b680d90c63c3ec15dce6e1e59e6eb1cb1eea2
Andrew Menning <menning23@gmail.com>
2015-10-04 10:05:05 -0400

    Lesson 54 Creating a Find and Replace Applications Part 1

---
+++ phptutorial/54_Creating_a_Find_and_Replace_Application_Part_1/index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+?>
+
+<form action="index.php" method="POST">
+
+<textarea name="text" rows="6" cols="30"></textarea><br><br>
+Search for:<br>
+<input type="text" name="searchfor"><br><br>
+
+Replace with:<br>
+<input type="text" name="replacewith"><br><br>
+
+<input type="submit" value="Find and Replace">
+
+
+</form>
