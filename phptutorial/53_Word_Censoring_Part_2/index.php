4b62f23f06d646339b3ee6fd55bb5df68090787b
Andrew Menning <menning23@gmail.com>
2015-10-04 09:44:20 -0400

    Lesson 53 Word Censoring Part 2

---
+++ phptutorial/53_Word_Censoring_Part_2/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$find = array('alex','billy','dale');
+$replace = array('a**x','b***y','d**e');
+
+if(isset($_POST['user_input']) && !empty($_POST['user_input'])){
+	$user_input = $_POST['user_input'];
+	$user_input_lc = strtolower($user_input);
+	$user_input_new = str_ireplace($find, $replace, $user_input);
+	echo $user_input_new;
+}
+
+?>
+
+<hr>
+
+<form action="index.php" method="POST">
+	<textarea name="user_input" rows="6" cols="30"><?php echo $user_input; ?></textarea><br><br>
+	<input type="submit" value="submit">
+</form>
