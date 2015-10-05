a8c691e61c8ffe6e4f5a178cc79917581a5a85f7
Andrew <menning23@gmail.com>
2015-10-05 12:46:56 -0400

    Lesson 71 Working with Form Data

---
+++ phptutorial/71_Working_with_Form_Data/index.php
@@ -0,0 +1,10 @@
+<?php
+
+?>
+
+<form action="index.php" method="GET">
+	Day: <input type="text" name="day"><br>
+	Date: <input type="text" name="date"><br>
+	Year: <input type="text" name="year"><br><br>
+	<input type="submit" value="Submit">
+</form>
