2179c0797b197399bd60970f79dfacde00f054ee
Andrew <menning23@gmail.com>
2015-10-09 16:10:43 -0400

    Lesson 88 Uploading Files The Basics Part 2

---
+++ phptutorial/88_Uploading_Files_The_Basics_Part_2/upload.php
@@ -0,0 +1,28 @@
+<?php
+
+$filename = $_FILES['file']['name'];
+$size = $_FILES['file']['size'];
+$type = $_FILES['file']['type'];
+
+$temp_name = $_FILES['file']['tmp_name'];
+
+$error = $_FILES['file']['error']; //returns 0 if there is no error
+
+if(isset($filename)){
+	if(!empty($filename)){
+		//move file from temporary location to specified location
+		$location = 'uploads/';
+		if(move_uploaded_file($temp_name,$location.$filename)){
+			echo 'Uploaded!';
+		}
+	}else{
+		echo 'Please choose a file.';
+	}
+}
+
+?>
+
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+	<input type="file" name="file" ><br><br>
+	<input type="submit" value="Submit">	
+</form>
