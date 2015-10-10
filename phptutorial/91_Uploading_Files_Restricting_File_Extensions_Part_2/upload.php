110748f2a212c6ef0bc25f6abda1e4725b75ab19
Andrew Menning <menning23@gmail.com>
2015-10-09 20:56:24 -0400

    Lesson 91 Uploading Files Restricting File Extensions Part 2

---
+++ phptutorial/91_Uploading_Files_Restricting_File_Extensions_Part_2/upload.php
@@ -0,0 +1,36 @@
+<?php
+
+@$filename = $_FILES['file']['name'];
+@$size = $_FILES['file']['size'];
+@$type = $_FILES['file']['type'];
+
+@$extension = strtolower(substr($filename, strpos($filename, '.')+1));
+
+@$temp_name = $_FILES['file']['tmp_name'];
+
+@$error = $_FILES['file']['error']; //returns 0 if there is no error
+
+if(isset($filename)){
+	if(!empty($filename)){
+		if(($extension==='jpg'||$extension==='jpeg') && $type==='image/jpeg'){
+			//move file from temporary location to specified location
+			$location = 'uploads/';
+			if(move_uploaded_file($temp_name,$location.$filename)){
+				echo 'Uploaded!';
+			}else{
+				echo 'There was an error.';
+			}		
+		}else{
+			echo 'File must be jpg/jpeg.';
+		}
+	}else{
+		echo 'Please choose a file.';
+	}
+}	
+
+?>
+
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+	<input type="file" name="file" ><br><br>
+	<input type="submit" value="Submit">	
+</form>
