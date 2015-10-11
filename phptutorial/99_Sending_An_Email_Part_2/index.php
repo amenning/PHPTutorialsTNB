95373641a9e71540b36740661d612cf27554eae7
Andrew Menning <menning23@gmail.com>
2015-10-11 12:57:34 -0400

    Lesson 99 Sending an Email Part 2

--- phptutorial/99_Sending_An_Email_Part_2/index.php
+++ phptutorial/99_Sending_An_Email_Part_2/index.php
@@ -2,11 +2,12 @@
 $to = 'alex@phpacademy.org'
 $subject = 'This is an email.';
 $body = 'This is a test email'."\n\n".'Hope you got it.';
-$headers = 'From: someone@phpacademy.org';
+$headers = 'From: phpacademy <someone@phpacademy.org>';
 
 if(mail($to, $subject, $body, $headers)){
 	echo 'Email has been sent to '.$to;
 }else{
 	echo 'There was an error sending the email.';
 }
+
 ?>
