9f3f77be15d137594fb6d49b8c76d8978d239be4
Andrew Menning <menning23@gmail.com>
2015-10-17 09:45:10 -0400

    Lesson 131 LIKE with a Search Engine Example Part 2

---
+++ phptutorial/131_LIKE_With_a_Search_Engine_Example_Part_2/index.php
@@ -0,0 +1,11 @@
+<?php
+require 'connect.inc.php';
+
+
+
+?>
+
+<form action="index.php" method="POST">
+	Name: <input type="text" name="search_name"> <input type="submit" value="Search">
+</form>
+
