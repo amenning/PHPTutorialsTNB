17e1e3d804619a5b5ace345a8886ed46e27f9a7b
Andrew Menning <menning23@gmail.com>
2015-10-20 20:46:16 -0400

    Lesson 146 Registration Form Part 3

---
+++ phptutorial/146_Registration_Form_Part_3/register.php
@@ -0,0 +1,51 @@
+<?php
+require 'core.inc.php';
+require 'connect.inc.php';
+
+if(!loggedin()){
+	
+if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['password_again']) && isset($_POST['firstname']) && isset($_POST['surname'])){
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$password_again = $_POST['password_again'];
+	$firstname = $_POST['firstname'];
+	$surname = $_POST['surname'];
+	
+	if(!empty($username) && !empty($password) && !empty($password_again) && !empty($firstname) && !empty($surname)){
+		if($password!==$password_again){
+			echo 'Passwords do not match.';
+		}else{
+			$query = "SELECT `username` FROM `users_135` WHERE `username`='".mysql_real_escape_string($username)."'";
+			$query_run = mysql_query($query);
+			
+			if(mysql_num_rows($query_run)==1){
+				echo 'The username '.$username.' already exists.';
+			}else{
+				echo 'Ok.';
+			}
+		}
+	}else{
+		echo 'All fields are required.';
+	}
+	
+}	
+	
+	
+?>
+
+<form action="register.php" method="POST">
+	Username: <br> <input type="text" name="username" value="<?php if(!empty($username)){echo $username;}?>"><br><br>
+	Password: <br> <input type="password" name="password"><br><br>
+	Password again: <br> <input type="password" name="password_again"><br><br>
+	Firstname: <br> <input type="text" name="firstname" value="<?php if(!empty($firstname)){echo $firstname;}?>"><br><br>
+	Surname: <br> <input type="text" name="surname" value="<?php if(!empty($surname)){echo $surname;}?>"><br><br>
+	<input type="submit" value="Register">
+</form>
+
+<?php
+}else if(loggedin()){
+	echo 'You\'re already registered and logged in.';
+}
+
+
+?>
