26feb648ac258a8cdea6d8bc5e2eaff5836c2853
Andrew Menning <menning23@gmail.com>
2015-10-25 18:19:02 -0400

    Lesson 172 Loading in File Contents to DIV Part 1

---
+++ phptutorial/172_Loading_in_File_Contents_to_DIV_Part_1/index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+
+<script type="text/javascript">
+
+function load(){
+	alert('Works.');
+}
+
+
+</script>
+
+</head>
+
+<body>
+
+<input type="submit" onclick="load();">
+
+<div id="adiv"></div>
+
+</body>
+</html>
