852c2b670b55543ec8269eba8eca287ed1ee7653
Andrew Menning <menning23@gmail.com>
2015-10-25 19:24:13 -0400

    Lesson 175 Auto Suggest Application Part 1

---
+++ phptutorial/175_Auto_Suggest_Application_Part_1/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+
+</head>
+
+<body>
+
+<form id="search" name="search">
+	Type a name:<br>
+	<input type="text" name="keyword" onkeydown="findmatch();">
+</form>
+
+<div id=""></div>
+
+</body>
+</html>
