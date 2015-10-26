5e2ad3a348a5f166bbf5063846ecb6819720fc5e
Andrew Menning <menning23@gmail.com>
2015-10-25 20:21:30 -0400

    Lesson 181 POSTing Data Part 2

---
+++ phptutorial/181_POSTing_Data_Part_2/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<script type="text/javascript">
+
+function insert(){
+	if(window.XMLHttpRequest){
+		xmlhttp = new XMLHttpRequest();
+	}else{
+		xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+	}
+	
+	
+}
+
+
+</script>
+</head>
+
+<body>
+
+Insert: <input type="text" id="text"> <input type="button" value="Submit" onclick="insert();">
+
+
+</body>
+</html>
