ad58260f97283daf0e84d6a49f630d090218132e
Andrew Menning <menning23@gmail.com>
2015-10-25 20:15:34 -0400

    Lesson 180 POSTing Data Part 1

---
+++ phptutorial/180_POSTing_Data_Part_1/index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<script type="text/javascript">
+
+function insert(){
+	text_value = getElementById('text').value;
+}
+
+
+</script>
+</head>
+
+<body>
+
+Insert: <input type="text" name="text"> <input type="button" value="Submit" onclick="insert();">
+
+
+</body>
+</html>
