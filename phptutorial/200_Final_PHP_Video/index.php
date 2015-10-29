d1a91269b0a7f0475bf5e3fc2d636324ab2f086d
Andrew Menning <menning23@gmail.com>
2015-10-28 20:03:57 -0400

    Lesson 200 Final PHP Video

--- phptutorial/200_Final_PHP_Video/index.php
+++ phptutorial/200_Final_PHP_Video/index.php
@@ -2,6 +2,7 @@
 
 class DatabaseConnect {
 	public function __construct($db_host, $db_username, $db_password){
+		echo 'Attempting connection... ';
 		if (!@$this->Connect($db_host, $db_username, $db_password)){
 			echo 'Connection failed';
 		}else if (@$this->Connect($db_host, $db_username, $db_password)){
@@ -10,7 +11,11 @@ class DatabaseConnect {
 	}
 	
 	public function Connect($db_host, $db_username, $db_password){
-		return true;
+		if(!@mysql_connect($db_host,$db_username,$db_password)){
+			return false;
+		}else {
+			return true;
+		}
 	}
 }
 
